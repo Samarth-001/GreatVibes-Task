d977bbf9558a1468bc8bc7cfd354e4fa99774916
Samarth-001 <samarthagl2002@gmail.com>
2023-05-19 18:27:49 +0530

    Styling error fix

--- src/styles.js
+++ src/styles.js
@@ -1,20 +1,20 @@
 // This contains all the styles as the variables and are passed inside the application
 
 const styles = {
-    "fontnormal": "normal",
-    "fontheavy": "medium",
+    fontnormal: "normal",
+    fontheavy: "medium",
 
-    "textsmall": "sm",
-    "textmedium": "base",
-    "textbig": "2xl",   
+    textsmall: "sm",
+    textmedium: "base",
+    textbig: "2xl",   
 
-    "cardcolor": "#FFFFFF",   
-    "cardborder": "#E6E6E6",   
-    "primarycolor": "#1597E4",   
-    "fontdarkcolor": "#212121",   
-    "fontlightcolor": "#FAFAFA",   
-    "placeholder": "#7A7A7A",   
-    "error": "#D86161",   
+    cardcolor: "#FFFFFF",   
+    cardborder: "#E6E6E6",   
+    primarycolor: "#1597E4",   
+    fontdarkcolor: "#212121",   
+    fontlightcolor : "#FAFAFA",   
+    placeholder: "#7A7A7A",   
+    error: "#D86161",   
 }
 
 export default styles
